b052f540ede43a3da16def88266f51ba0762ce2d
Adrián <arodriba@ull.edu.es>
2016-04-07 23:21:40 +0100

    CSV JS script improved

--- js/csv.js
+++ js/csv.js
@@ -3,10 +3,12 @@
   // See http://en.wikipedia.org/wiki/Comma-separated_values
 
   var regexp = /"((?:[^"\\]|\\.)*)"|([^,\s]+)|,\s*(?=,|$)|^\s*,/g
+
   exports.calculate = function(original) {
     var lines = original.split(/\n+\s*/);
     var commonLength = lines[0].match(regexp).length;
     var r = [];
+
     var removeQuotes = function(field) {
       var removecomma = field.replace(/,\s*$/, '');
       var remove1stquote = removecomma.replace(/^\s*"/, '');
@@ -14,27 +16,27 @@
       var removeescapedquotes = removelastquote.replace(/\\"/, '"');
       return removeescapedquotes;
     };
-
+if (window.localStorage) {
+  localStorage.original = original;
+}
     for (var t in lines) {
       var temp = lines[t];
       var m = temp.match(regexp);
       var result = [];
       var error = false;
 
-      // skip empty lines and comments
-      if (temp.match(/(^\s*$)|(^#.*)/)) continue; 
+      // Skip empty lines and commentaries
+      if (temp.match(/(^\s*$)|(^#.*)/)) continue;
       if (m) {
         result = m.map(removeQuotes);
         error = (commonLength != m.length);
         var rowclass = error? 'error' : '';
         r.push({ value: result, rowClass: rowclass });
       }
-      else {
-        var errmsg = 'La fila "' + temp + '" no es un valor de CSV permitido.';
-        r.push({value: errmsg.split("").splice(commonLength), rowClass: 'error'});
-      }
     }
+    var template = fillTable.innerHTML;
+    finaltable.innerHTML = _.template(template, {items: r});
+
     return r;
   };
 })(this);
-
