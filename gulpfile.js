44161ee6bb4fccd54c64d1305016ce1f6f882763
Adrián <arodriba@ull.edu.es>
2016-04-07 22:42:58 +0100

    Gulpfile improved

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@
 
 var gulp = require('gulp');
 var watch = require('gulp-watch'); // Watch to have background tasks executing when some event is triggered
-var htmlmin = require('gulp-htmlmin');
+var htmlmin = require('gulp-minify-html');
+var cssmin = require('gulp-minify-css');
 var ghPages = require('gulp-gh-pages');		// Used to update the gh-pages branch
 var clean = require('gulp-clean');	 // To clean the public directory
 var uglify = require('gulp-uglify');     // To uglify the codes
@@ -29,10 +30,18 @@ gulp.task('sass', function () {
     .pipe(gulp.dest(output.sass));
 });
 
-gulp.task('minified-js', function() {
-  return gulp.src(input.js)
-    .pipe(uglify())
-    .pipe(gulp.dest(output.js));
+gulp.task('minify-all', function () {
+  gulp.src('js/csv.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('minified'));
+
+  gulp.src('./index.html')
+  .pipe(htmlmin())
+  .pipe(gulp.dest('./minified/'))
+
+  gulp.src('./css/*.css')
+  .pipe(cssmin({keepBreaks:true}))
+  .pipe(gulp.dest('./minified/'))
 });
 
 gulp.task('minify-html', function() {
@@ -41,11 +50,13 @@ gulp.task('minify-html', function() {
     .pipe(gulp.dest(output.html));
 });
 
-gulp.task('generate-pages', function() {
-  return gulp.src(outDir + '**/*')
-    .pipe(ghPages('.'));
+// Deploy to gh pages
+gulp.task('deploy', function() {
+  return gulp.src('./minified/**/*')
+    .pipe(ghPages());
 });
 
+// Clean everything
 gulp.task('clean', function () {
 	return gulp.src(outDir, {read: false})
 		.pipe(clean());
@@ -57,4 +68,23 @@ gulp.task('watch', function () {
   gulp.watch(input.sass, ['sass']);
 });
 
-gulp.task('default', ['minify-html', "minified-js", "sass"]);  // Default tasks loads everything again
+// Run the tests
+gulp.task('test', function() {
+  return gulp.src([])
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }))
+    .on('error', function(err) {
+      throw err;
+    });
+});
+
+// Default tasks
+gulp.task('default', ['minify-all'], function() {
+  gulp.src([])
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'watch'
+    }));
+});
