3699afe6d5a152cc7d926b7c79eb0bb3a6766c26
Adrián Rodríguez Bazaga <alu0100826456@ull.edu.es>
2016-04-08 01:58:44 +0100

    Fix

--- js/csv.js
+++ js/csv.js
@@ -1,52 +1,55 @@
 (function(exports) {
-    "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
-    // See http://en.wikipedia.org/wiki/Comma-separated_values
+  "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
+  // See http://en.wikipedia.org/wiki/Comma-separated_values
 
-    var regexp = /"((?:[^"\\]|\\.)*)"|([^,\s]+)|,\s*(?=,|$)|^\s*,/g
+  var regexp = /"((?:[^"\\]|\\.)*)"|([^,\s]+)|,\s*(?=,|$)|^\s*,/g
+  exports.calculate = function(original) {
+    var lines = original.split(/\n+\s*/);
+    var firstLine;
 
-    exports.calculate = function(original) {
-        //var a =original.value;
-        var lines = original.split(/\n+\s*/);
-        var commonLength = lines[0].match(regexp).length;
-        var r = [];
+    for (var i in lines)
+      if (lines[i].match(regexp)) {
+        firstLine = i;
+        var commonLength = lines[i].match(regexp).length;
+        break;
+      }
 
-        var removeQuotes = function(field) {
-            var removecomma = field.replace(/,\s*$/, '');
-            var remove1stquote = removecomma.replace(/^\s*"/, '');
-            var removelastquote = remove1stquote.replace(/"\s*$/, '');
-            var removeescapedquotes = removelastquote.replace(/\\"/, '"');
-            return removeescapedquotes;
-        };
-        if (window.localStorage) {
-            localStorage.original = original;
-        }
-        for (var t in lines) {
-            var temp = lines[t];
-            var m = temp.match(regexp);
-            var result = [];
-            var error = false;
-
-            // skip empty lines and comments
-            if (temp.match(/(^\s*$)|(^#.*)/)) continue;
-            if (m) {
-                result = m.map(removeQuotes);
-                error = (commonLength != m.length);
-                var rowclass = error ? 'error' : '';
-                r.push({
-                    value: result,
-                    rowClass: rowclass
-                });
-            }
-            /*else {
-              var errmsg = 'La fila "' + temp + '" no es un valor de CSV permitido.';
-              r.push({value: errmsg.split("").splice(commonLength), rowClass: 'error'});
-            }*/
-        }
-        var template = fillTable.innerHTML;
-        finaltable.innerHTML = _.template(template, {
-            items: r
-        });
-
-        return r;
+    var r = [];
+    var removeQuotes = function(field) {
+      var removecomma = field.replace(/,\s*$/, '');
+      var remove1stquote = removecomma.replace(/^\s*"/, '');
+      var removelastquote = remove1stquote.replace(/"\s*$/, '');
+      var removeescapedquotes = removelastquote.replace(/\\"/, '"');
+      return removeescapedquotes;
     };
+
+    for (var t in lines) {
+      var temp = lines[t];
+      var m = temp.match(regexp);
+      var result = [];
+      var error = false;
+      var first = false;
+
+      // skip empty lines and comments
+      if (temp.match(/(^\s*$)|(^#.*)/)) continue;
+      if (m) {
+        result = m.map(removeQuotes);
+        error = (commonLength != m.length);
+        first = (firstLine == t);
+        var rowclass = error ? 'error' : '';
+        rowclass = first ? 'first' : rowclass;
+        r.push({
+          value: result,
+          rowClass: rowclass
+        });
+      } else {
+        var errmsg = 'La fila "' + temp + '" no es un valor de CSV permitido.';
+        r.push({
+          value: errmsg.split("").splice(commonLength),
+          rowClass: 'error'
+        });
+      }
+    }
+    return r;
+  };
 })(this);
