baf7bacc0688c001b0f9a91a91446fa03c14e49c
Adrian R. Bazaga <alu0100826456@ull.edu.es>
2016-04-10 23:01:06 +0100

    Some fixing

--- public/test/csv-testing.js
+++ public/test/csv-testing.js
@@ -65,4 +65,36 @@ describe('CSV Testing', function() {
       expect(aux[2].value).to.deep.equal(['red','62','pencil']);
     });
   });
+  
+  describe('Testing edge cases', function () {
+    it('Testing empty input', function () {
+	  this.original = "";
+      var aux = calculate(this.original);
+      aux.should.have.length(0);
+    });
+
+    it('Testing several jump lines', function () {
+      this.original = "\n\n\n";
+      var aux = calculate(this.original);
+      aux.should.have.length(0);
+    });
+
+    it('Testing several jump lines + dirty line', function () {
+      this.original = "\n\nfasd\n";
+      var aux = calculate(this.original);
+      aux[0].value[0].should.equal('fasd');
+    });
+
+    it('Testing if it correctly fails on bad input (not following the correct format)', function () {
+      this.original = "param1,param2\nbadline\ncorrect, line\n";
+      var aux = calculate(this.original);
+      aux[0].value[0].should.equal('param1');
+      aux[0].value[1].should.equal('param2');
+      aux[1].rowClass.should.equal('error');
+      aux[1].value[0].should.equal('badline');
+      aux[1].value.should.have.length(1);
+      aux[2].value[0].should.equal('correct');
+      aux[2].value[1].should.equal('line');
+    });
+  });
 });
